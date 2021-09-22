1b317f0cae5011292276d4747b15ca5616c99f88
Benjamin Heath <80618413+benjiheath@users.noreply.github.com>
2021-09-22 13:24:17 +1000

    feat(Cart): add purchase button

--- src/client/Cart/Cart.tsx
+++ src/client/Cart/Cart.tsx
@@ -1,6 +1,7 @@
 import CartItem from './CartItem/CartItem';
 import { Wrapper } from './Cart.styles';
 import { CartItemType } from '../App';
+import { Button } from '@material-ui/core';
 
 type Props = {
   cartItems: CartItemType[];
@@ -16,15 +17,13 @@ const Cart: React.FC<Props> = ({ cartItems, addToCart, removeFromCart }) => {
     <Wrapper>
       <h2>Your Shopping Cart</h2>
       {cartItems.length === 0 ? <p>No items in cart.</p> : null}
-      {cartItems.map(item => (
-        <CartItem
-          key={item.id}
-          item={item}
-          addToCart={addToCart}
-          removeFromCart={removeFromCart}
-        />
+      {cartItems.map((item) => (
+        <CartItem key={item.id} item={item} addToCart={addToCart} removeFromCart={removeFromCart} />
       ))}
       <h2>Total: ${calculateTotal(cartItems).toFixed(2)}</h2>
+      <Button variant='contained' color='primary'>
+        Purchase
+      </Button>
     </Wrapper>
   );
 };
