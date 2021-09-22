7030e57244f757359c591d316b96ca15482a6dd1
Benjamin Heath <80618413+benjiheath@users.noreply.github.com>
2021-09-22 12:06:18 +1000

    feat(Dialog): add dialog functionality

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -23,33 +23,24 @@ export type CartItemType = {
   amount: number;
 };
 
-
-const getCheeses = async (): Promise<CartItemType[]> =>
-  await (await fetch(`api/cheeses`)).json();
+const getCheeses = async (): Promise<CartItemType[]> => await (await fetch(`api/cheeses`)).json();
 
 const App = () => {
   const [cartOpen, setCartOpen] = useState(false);
   const [cartItems, setCartItems] = useState([] as CartItemType[]);
-  const { data, isLoading, error } = useQuery<CartItemType[]>(
-    'cheeses',
-    getCheeses
-  );
-  console.log(data);
+  const [itemDialogOpen, setItemDialogOpen] = useState(false);
+  const { data, isLoading, error } = useQuery<CartItemType[]>('cheeses', getCheeses);
+  console.log('data', data);
 
-  const getTotalItems = (items: CartItemType[]) =>
-    items.reduce((ack: number, item) => ack + item.amount, 0);
+  const getTotalItems = (items: CartItemType[]) => items.reduce((ack: number, item) => ack + item.amount, 0);
 
   const handleAddToCart = (clickedItem: CartItemType) => {
-    setCartItems(prev => {
+    setCartItems((prev) => {
       // 1. Is the item already added in the cart?
-      const isItemInCart = prev.find(item => item.id === clickedItem.id);
+      const isItemInCart = prev.find((item) => item.id === clickedItem.id);
 
       if (isItemInCart) {
-        return prev.map(item =>
-          item.id === clickedItem.id
-            ? { ...item, amount: item.amount + 1 }
-            : item
-        );
+        return prev.map((item) => (item.id === clickedItem.id ? { ...item, amount: item.amount + 1 } : item));
       }
       // First time the item is added
       return [...prev, { ...clickedItem, amount: 1 }];
@@ -57,7 +48,7 @@ const App = () => {
   };
 
   const handleRemoveFromCart = (id: number) => {
-    setCartItems(prev =>
+    setCartItems((prev) =>
       prev.reduce((ack, item) => {
         if (item.id === id) {
           if (item.amount === 1) return ack;
@@ -73,61 +64,42 @@ const App = () => {
   if (error) return <div>Something went wrong ...</div>;
 
   return (
-
     <Wrapper>
-      <StyledAppBar position="static">
+      <StyledAppBar position='static'>
         <Toolbar>
-          <Grid
-            container
-            direction="row"
-            justify="space-between"
-            alignItems="center"
-          >
+          <Grid container direction='row' justify='space-between' alignItems='center'>
             <StyledButton>
               <RestoreIcon />
-              <Typography variant="subtitle2">
-                Recent Purchases
-              </Typography>
+              <Typography variant='subtitle2'>Recent Purchases</Typography>
             </StyledButton>
 
-            <HeaderTypography variant="h3" noWrap>
+            <HeaderTypography variant='h3' noWrap>
               Welcome to Patient Zero's Cheeseria
             </HeaderTypography>
 
             <StyledButton onClick={() => setCartOpen(true)}>
-              <Badge
-                badgeContent={getTotalItems(cartItems)}
-                color='error'
-                data-cy="badge-count">
+              <Badge badgeContent={getTotalItems(cartItems)} color='error' data-cy='badge-count'>
                 <AddShoppingCartIcon />
               </Badge>
 
-              <Typography variant="subtitle2">
-                Cart
-              </Typography>
+              <Typography variant='subtitle2'>Cart</Typography>
             </StyledButton>
-
           </Grid>
         </Toolbar>
       </StyledAppBar>
 
       <Drawer anchor='right' open={cartOpen} onClose={() => setCartOpen(false)}>
-        <Cart
-          cartItems={cartItems}
-          addToCart={handleAddToCart}
-          removeFromCart={handleRemoveFromCart}
-        />
+        <Cart cartItems={cartItems} addToCart={handleAddToCart} removeFromCart={handleRemoveFromCart} />
       </Drawer>
 
       <Grid container spacing={3}>
-        {data?.map(item => (
+        {data?.map((item) => (
           <Grid item key={item.id} xs={12} sm={4}>
             <Item item={item} handleAddToCart={handleAddToCart} />
           </Grid>
         ))}
       </Grid>
     </Wrapper>
-
   );
 };
 
--- src/client/Cart/Item/Item.tsx
+++ src/client/Cart/Item/Item.tsx
@@ -1,25 +1,50 @@
 import Button from '@material-ui/core/Button';
+import { useState } from 'react';
 // Types
 import { CartItemType } from '../../App';
 // Styles
 import { Wrapper } from './Item.styles';
+import ItemDialog from './ItemDialog';
+
+export type HandleAddToCart = (clickedItem: CartItemType) => void;
 
 type Props = {
   item: CartItemType;
-  handleAddToCart: (clickedItem: CartItemType) => void;
+  handleAddToCart: HandleAddToCart;
 };
 
-const Item: React.FC<Props> = ({ item, handleAddToCart }) => (
-  <Wrapper>
-    <img src={item.image} alt={item.title} />
-    <div>
-      <h3>{item.title}</h3>
-      <h3>${item.price}</h3>
-    </div>
-    <Button
-      onClick={() => handleAddToCart(item)}
-      data-cy={`add-to-cart-${item.id}`}>Add to cart</Button>
-  </Wrapper>
-);
+const Item: React.FC<Props> = ({ item, handleAddToCart }) => {
+  const [itemDialogOpen, setItemDialogOpen] = useState(false);
+
+  const handleItemClick = () => {
+    setItemDialogOpen(true);
+  };
+
+  const handleCloseDialog = () => {
+    setItemDialogOpen(false);
+  };
+
+  return (
+    <>
+      <ItemDialog item={item} open={itemDialogOpen} handleClose={handleCloseDialog} handleAddToCart={handleAddToCart} />
+      <Wrapper onClick={() => handleItemClick()}>
+        <img src={item.image} alt={item.title} />
+        <div>
+          <h3>{item.title}</h3>
+          <h3>${item.price}</h3>
+        </div>
+        <Button
+          onClick={(e) => {
+            e.stopPropagation();
+            handleAddToCart(item);
+          }}
+          data-cy={`add-to-cart-${item.id}`}
+        >
+          Add to cart
+        </Button>
+      </Wrapper>
+    </>
+  );
+};
 
 export default Item;
