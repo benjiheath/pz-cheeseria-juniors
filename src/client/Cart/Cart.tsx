ad7cb8d4c31b95d1b440abd89f46670a30b1d894
Benjamin Heath <80618413+benjiheath@users.noreply.github.com>
2021-09-22 14:58:20 +1000

    feat(Cart): add purchase handler (POST fetch)

--- src/client/Cart/Cart.tsx
+++ src/client/Cart/Cart.tsx
@@ -13,6 +13,18 @@ const Cart: React.FC<Props> = ({ cartItems, addToCart, removeFromCart }) => {
   const calculateTotal = (items: CartItemType[]) =>
     items.reduce((ack: number, item) => ack + item.amount * item.price, 0);
 
+  const purchaseHandler = async () => {
+    const res = await fetch('api/cart', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(cartItems),
+    });
+
+    console.log('res:', res);
+  };
+
   return (
     <Wrapper>
       <h2>Your Shopping Cart</h2>
@@ -21,7 +33,7 @@ const Cart: React.FC<Props> = ({ cartItems, addToCart, removeFromCart }) => {
         <CartItem key={item.id} item={item} addToCart={addToCart} removeFromCart={removeFromCart} />
       ))}
       <h2>Total: ${calculateTotal(cartItems).toFixed(2)}</h2>
-      <Button variant='contained' color='primary'>
+      <Button variant='contained' color='primary' onClick={() => purchaseHandler()}>
         Purchase
       </Button>
     </Wrapper>
